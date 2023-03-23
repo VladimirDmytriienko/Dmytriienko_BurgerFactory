3341d6b930834f1fe62751a459754117d63d60a3
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2023-03-23 16:26:49 +0200

    map commit check

--- js/location.js
+++ js/location.js
@@ -1,25 +1,22 @@
 function initMap() {
-
-  // Получаем div с классом "gm-style" и задаем ему border-radius
-
-
-
-    var location = {lat: 46.524297339046726, lng: 6.633018642329806}; // Координаты маркера
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 17, // Масштаб карты
-      center: location // Центр карты
-    });
-    var marker = new google.maps.Marker({
-      position: location,
-      map: map,
-      title: 'Маркер',
-      icon: './icons/burger_location.png'
-    });
-    var gmStyleDiv = document.getElementsByClassName('gm-style')[0];
-gmStyleDiv.style.borderRadius = '30px';
+  var location = {lat: 46.524297339046726, lng: 6.633018642329806};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: location,
+    styles: [
+      {
+        "stylers": [
+          {
+            "radius": "30px"
+          }
+        ]
+      }
+    ]
+  });
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    title: 'Маркер',
+    icon: './icons/burger_location.png'
+  });
 }
-
-var divElement = document.getElementById("map");
-
-divElement.style.borderRadius = "30px";
-
