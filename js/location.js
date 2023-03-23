54b28e60c851a0679833878eec1abb74b144450c
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2023-03-23 13:34:25 +0200

    try to fix border on iphone

--- js/location.js
+++ js/location.js
@@ -1,8 +1,5 @@
 function initMap() {
 
-        
-
-
     var location = {lat: 46.524297339046726, lng: 6.633018642329806}; // Координаты маркера
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 17, // Масштаб карты
